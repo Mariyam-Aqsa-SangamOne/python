172d2910171818b71ccdf0c9a69db05b3887aa0f
Mariyam Aqsa Rafiq <mariyamaqsa.sangamone@gmail.com>
2026-06-05 20:12:26 +0530

    Quiz Program with function quiz(fname)

--- Quiz1.py
+++ Quiz1.py
@@ -1,45 +1,41 @@
-def read_file():
-    f1=open("GK1.txt","r")
+def quiz(fname):
+    countries=[]
+    capitals=[]
+    responses=[]
+    marks=[]
+
+    f1=open(fname,"r")
     for i in range(0,10,1):
-        n1=f1.readline()
-        n1=n1.strip()
+        s1=f1.readline()
+        n1=s1.strip()
         l1=n1.split(",")
-        list1.append(l1[0])
-        list2.append(l1[1])
+        countries.append(l1[0])
+        capitals.append(l1[1])
 
     f1.close()
-
-def display_ques():
     for i in range(0,10,1):
-        q1="What is the capital of "+list1[i]
+        q1="What is the capital of "+countries[i]
         ans=input(q1+"?")
-        list3.append(ans)
+        responses.append(ans)
     print()
 
-def check_ans():
     for i in range(0,10,1):
-        if list3[i].lower()==list2[i].lower():
-            list4.append(10)
+        if responses[i].strip().lower()==capitals[i].strip().lower():
+            marks.append(10)
         else:
-            list4.append(0)
-
-def display_result():
-    total=sum(list4)
+            marks.append(0)
     print()
+
+    total=sum(marks)
     print("Total marks:",total)
-    print()
-    print("Wrongly Answered Questions:")
-    for i in range(0,10,1):
-        if list4[i]==0:
-            print("What is the capital of ",list1[i],"?")
-            print("Correct answer: ",list2[i])
-
-#Main 
-list1=[]
-list2=[]
-list3=[]
-list4=[]
-read_file()
-display_ques()
-check_ans()
-display_result()
+    if(total==100):
+        print("Congratulations!")
+    else:
+        print("Wrongly Answered Questions:")
+        for i in range(0,10,1):
+            if marks[i]==0:
+                print("What is the capital of ",countries[i],"?")
+                print("Correct answer: ",capitals[i])
+                print()
+        print()
+quiz("GK1.txt")
